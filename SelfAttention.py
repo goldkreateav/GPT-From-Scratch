75ddb2920c3159c543060d450a6f3f77a846c38c
Egor <arshegor@gmail.com>
2026-03-15 10:20:57 +0300

    MultiHeadAttention class

--- SelfAttention.py
+++ SelfAttention.py
@@ -1,12 +1,11 @@
 import torch
-
-class SelfAttention_v1(torch.nn.Module):
+import torch.nn as nn
+class SelfAttention_v1(nn.Module):
     def __init__(self, d_in, d_out):
         super().__init__()
-        torch.manual_seed(123)
-        self.W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-        self.W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-        self.W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
 
     def forward(self, x):
         query = x @ self.W_query
@@ -18,13 +17,12 @@ class SelfAttention_v1(torch.nn.Module):
         attn_weights = torch.softmax(attn_score / d_k ** 0.5, dim=-1)
         return attn_weights @ values
 
-class SelfAttention_v2(torch.nn.Module):
+class SelfAttention_v2(nn.Module):
     def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
-        torch.manual_seed(123)
-        self.W_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
     def forward(self, x):
         queries = self.W_query(x)
@@ -35,14 +33,15 @@ class SelfAttention_v2(torch.nn.Module):
         d_k = keys.shape[-1]
         attn_weights = torch.softmax(attn_score / d_k ** 0.5, dim=-1)
         return attn_weights @ values
-class CasualAttention(torch.nn.Module):
+
+class CasualAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
         self.d_out = d_out
-        self.W_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.W_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.dropout = torch.nn.Dropout(dropout)
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, x):
@@ -58,9 +57,44 @@ class CasualAttention(torch.nn.Module):
         attn_weights = torch.softmax(attn_score / d_k ** 0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
         return attn_weights @ values
-class MultiHeadAttensionWrapper(torch.nn.Module):
+
+class MultiHeadAttensionWrapper(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_head, qkv_bias=False):
         super().__init__()
-        self.heads = torch.nn.ModuleList([CasualAttention(d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_head)])
+        self.heads = nn.ModuleList([CasualAttention(d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_head)])
     def forward(self, x):
         return torch.cat([head(x) for head in self.heads], dim=-1)
+
+class MultiHeadAttension(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.head_dim = d_out // num_heads
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.num_heads = num_heads
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        queries = self.W_query(x)
+        keys = self.W_key(x)
+        values = self.W_value(x)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
+        
+        attn_score = queries @ keys.transpose(2, 3)
+
+        attn_score.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        d_k = keys.shape[-1]
+        attn_weights = torch.softmax(attn_score / d_k ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+        return context_vec
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tiktoken
 from GPTDataset import GPTDatasetV1
 from torch.utils.data import Dataset, DataLoader
 import torch
-from SelfAttention import SelfAttention_v1, SelfAttention_v2, CasualAttention, MultiHeadAttensionWrapper
+from SelfAttention import SelfAttention_v1, SelfAttention_v2, CasualAttention, MultiHeadAttensionWrapper, MultiHeadAttension
 def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, 
                           shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")  
@@ -28,11 +28,10 @@ inputs = torch.tensor([
     [0.05, 0.80, 0.55]   # step (x^6)
 ])
 
-d_in = inputs.shape[-1]
-d_out = 1
+d_in = 768
+d_out = 768
 torch.manual_seed(123)
 batch = torch.stack((inputs, inputs), dim=0)
-context_length = batch.shape[1]
+context_length = 1024
 print(d_in, d_out, context_length)
-multihead = MultiHeadAttensionWrapper(d_in, d_out, context_length, 0.0, num_head=2)
-print(multihead(batch))
+multihead = MultiHeadAttension(d_in, d_out, context_length, 0.0, num_heads=12)
